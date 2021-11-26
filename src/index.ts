87968c2b15396f4717a1fdf530abf42cb5190cf3
CarlosUtLaleo <abrahamz@librerialeo.com.mx>
2021-11-26 13:14:16 -0600

    Ultima sesion typescript

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Producto, calculaISV } from './ejercicios/06-desestructuracion-function';
-
-const carritoCompras: Producto[] = [
-    {
-        desc: 'Telefono 1',
-        precio: 100,
-    },
-];
